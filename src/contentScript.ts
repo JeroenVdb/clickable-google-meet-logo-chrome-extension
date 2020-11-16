9dfb0d81624810b5361cc49c0e7ed9871feff503
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-11-16 13:25:47 +0100

    finetune permissions, we don't need activeTab

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,16 +1,17 @@
 function createClickableArea() {
 	const area = document.createElement('a');
 	area.setAttribute('href', createGoogleMeetHomeUrl());
-	area.setAttribute('style', 'position: fixed; width: 100px; height: 100px; z-index: 5000');
+	area.setAttribute('style', 'position: fixed; width: 400px; height: 64px; z-index: 5000');
 	return area;
 }
 
-function createGoogleMeetHomeUrl(): string {
+function createGoogleMeetHomeUrl() {
 	let currentUrl = new URL(window.location.href);
 	let homepageUrl = new URL(currentUrl.origin);
+	let author = currentUrl.searchParams.get('authuser');
 
-	if (currentUrl.searchParams.has('authuser')) {
-		homepageUrl.searchParams.append('authuser', currentUrl.searchParams.get('authuser')!)
+	if (author) {
+		homepageUrl.searchParams.append('authuser', author);
 	}
 
 	return homepageUrl.toString();
